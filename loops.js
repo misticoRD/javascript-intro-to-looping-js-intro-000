6aaebc404e8086c2b3c1152bcefe9585e6613c5c
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-10 17:03:00 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -12,20 +12,20 @@ function forLoop(array) {
 function whileLoop(n){
   while(n > 0) {
       console.log(--n);
-}  
+}
    return "done";
 }
 
 function doWhileLoop(num){
   let i = 0;
-   
+
   function incrementVariable() {
     i = i + 1;
     return i;
   }
-   
+
   do {
     console.log("I run once regardless.");
   } while (incrementVariable() < num);
-  
+
 }
